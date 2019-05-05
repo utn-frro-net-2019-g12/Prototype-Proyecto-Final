d88dd9a16731e8f5261300d531526a1d360bd86d
alereca <alejandrorecalde5@gmail.com>
2019-05-05 10:08:34 -0400

    Change Authorization Context name

--- WebApi/Models/IdentityModels.cs
+++ WebApi/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace WebApi.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AuthorizationContext", throwIfV1Schema: false)
         {
         }
         
